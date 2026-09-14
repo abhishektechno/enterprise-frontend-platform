78aea051491f1203cf2dc6d1202c6ae2b933dc56
Mokshada@231326 <abhishek11.net@gmail.com>
2026-09-14 12:19:01 +0530

    git commit -m "refactor(shell): establish frontend folder structure"

---
+++ apps/shell/src/app/App.js
@@ -0,0 +1,4 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+export function App() {
+    return (_jsxs("main", { children: [_jsx("h1", { children: "Enterprise Operations Platform" }), _jsx("p", { children: "Enterprise frontend architecture demonstration project." })] }));
+}
